032e08c89e919f08d7ff2376a01bd9bc2f6c635d
rodrigo debian <correo.rodrigo@gmail.com>
2020-08-05 11:15:31 -0500

    adding content

---
+++ Ro.Sql.Data/DataReaderHelpers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Xml;
+
+namespace Ro.Sql.Data
+{
+    public static class DataReaderHelpers
+    {
+        public static DateTime GetDate(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToDate(dr[key]);
+        }
+
+        public static string GetString(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToStr(dr[key]);
+        }
+
+        public static decimal GetDecilmal(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToDecilmal(dr[key]);
+        }
+
+        public static float GetFloat(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToFloat(dr[key]);
+        }
+
+        public static int GetInt(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToInt(dr[key]);
+        }
+
+        public static long GetLong(this IDataReader dr, string key, long ifNull = 0)
+        {
+            return MapperHelpers.ToLong(dr[key]);
+        }
+
+        public static Guid GetGuid(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToGuid(dr[key]);
+        }
+
+        public static Guid? ToGuidNullable(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToGuidNullable(dr[key]);
+        }
+
+        public static XmlDocument GetXml(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToXml(dr[key]);
+        }
+
+        public static T GetValue<T>(this IDataReader dr, string key)
+        {
+            return MapperHelpers.ToVal<T>(dr[key]);
+        }
+    }
+}
---
+++ Ro.Sql.Data/Database.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Ro.Sql.Data
+{
+    public class Database : IDatabaseAsync
+    {
+        private string ConnectionString { get; set; }
+        public Database(string connString)
+        {
+            this.ConnectionString = connString;
+        }
+
+        private DbConnection GetConnection()
+        {
+            return new SqlConnection(this.ConnectionString);
+        }
+
+        public Task<int> ExecuteNonQuery(DbCommand cmd)
+        {
+            return DatabaseTasks.ExecuteNonQueryAsync(cmd, GetConnection());
+        }
+
+        public Task<object> ExecuteScalar(DbCommand cmd)
+        {
+            return DatabaseTasks.ExecuteScalarAsync(cmd, GetConnection());
+        }
+
+        public Task ExecuteReader(DbCommand cmd, Action<IDataReader> action, CommandBehavior behavior = CommandBehavior.CloseConnection)
+        {
+            return DatabaseTasks.ExecuteReaderAsync(cmd, GetConnection(), action, behavior);
+        }
+
+        public Task<T> GetOneRow<T>(DbCommand cmd, Func<IDataReader, T> mapper)
+        {
+            return DatabaseTasks.GetOneRow(cmd, GetConnection(), mapper);
+        }
+
+        public Task<IEnumerable<T>> GetRows<T>(DbCommand cmd, Func<IDataReader, T> mapper)
+        {
+            return DatabaseTasks.GetRows(cmd, GetConnection(), mapper);
+        }
+
+        public Task<T> GetOneRowAsync<T>(DbCommand cmd, Func<IDataReader, Task<T>> mapper)
+        {
+            return DatabaseTasks.GetOneRowAsync(cmd, GetConnection(), mapper);
+        }
+
+        public Task<IEnumerable<T>> GetRowsAsync<T>(DbCommand cmd, Func<IDataReader, Task<T>> mapper)
+        {
+            return DatabaseTasks.GetRowsAsync(cmd, GetConnection(), mapper);
+        }
+    }
+}
